0917cf8dad96ab57617e304db5145353a235602e
will-code-for-food-and-water <marg667@live.de>
2018-09-12 15:28:01 +0200

    added @Repository annotation to silence intelliJ

--- src/main/java/com/WWI16AMA/backend_api/Member/MemberRepository.java
+++ src/main/java/com/WWI16AMA/backend_api/Member/MemberRepository.java
@@ -1,9 +1,9 @@
 package com.WWI16AMA.backend_api.Member;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-import com.WWI16AMA.backend_api.Member.Member;
-
+@Repository
 public interface MemberRepository extends CrudRepository<Member, Integer>{
 
 }
