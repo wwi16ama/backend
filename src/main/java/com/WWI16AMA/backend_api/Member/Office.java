0843e99f98bd3626f6ba549aceb7e42daad7e005
will-code-for-food-and-water <marg667@live.de>
2018-09-20 21:54:02 +0200

    Klassen großschreiben

--- src/main/java/com/WWI16AMA/backend_api/Member/Office.java
+++ src/main/java/com/WWI16AMA/backend_api/Member/Office.java
@@ -3,60 +3,57 @@ package com.WWI16AMA.backend_api.Member;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.IdentityHashMap;
-import java.util.List;
 
 @Entity
-public class Office{
+public class Office {
 
-public enum officeName {
-    FLUGWART("Flugwart"),
-    IMBETRIEBSKONTROLLTURMARBEITEND("ImBetriebskontrollturmArbeitend"),
-    KASSIERER("Kassierer"),
-    SYSTEMADMINISTRATOR("Systemadministrator"),
-    VORSTANDSVORSITZENDER("Vorstandsvorsitzender");
+    public enum OfficeName {
+        FLUGWART("Flugwart"),
+        IMBETRIEBSKONTROLLTURMARBEITEND("ImBetriebskontrollturmArbeitend"),
+        KASSIERER("Kassierer"),
+        SYSTEMADMINISTRATOR("Systemadministrator"),
+        VORSTANDSVORSITZENDER("Vorstandsvorsitzender");
 
-    @Enumerated(EnumType.STRING)
-    private String title;
+        @Enumerated(EnumType.STRING)
+        private String title;
 
-    officeName(String title) {
-        this.title = title.toUpperCase();
+        OfficeName(String title) {
+            this.title = title.toUpperCase();
+        }
+
+        public void setTitle(String title) {
+            this.title = title.toUpperCase();
+        }
+
+        @JsonValue
+        public String getTitle() {
+            return title;
+        }
+
+
     }
-
-    public void setTitle(String title) {
-        this.title = title.toUpperCase();
-    }
-
-    @JsonValue
-    public String getTitle() {
-        return title;
-    }
-
-
-}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Enumerated(EnumType.STRING)
-    @Column(name="office_name")
-    officeName officeName;
+    @Column(name = "office_name")
+    OfficeName officeName;
 
-    public Office.officeName getOfficeName() {
+    public OfficeName getOfficeName() {
         return officeName;
     }
 
-    public void setOfficeName(Office.officeName officeName) {
+    public void setOfficeName(OfficeName officeName) {
         this.officeName = officeName;
     }
 
-    public Office(Office.officeName officeName) {
+    public Office(OfficeName officeName) {
         this.officeName = officeName;
     }
 
-    Office(){
+    Office() {
 
     }
 }
