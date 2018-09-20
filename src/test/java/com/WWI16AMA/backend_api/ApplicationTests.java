ad949ce9a455f0d39d24fa9291f83eeeeb06db73
will-code-for-food-and-water <marg667@live.de>
2018-09-20 16:07:26 +0200

    Tests for POST, PUT, DELETE; no more printing

--- src/test/java/com/WWI16AMA/backend_api/ApplicationTests.java
+++ src/test/java/com/WWI16AMA/backend_api/ApplicationTests.java
@@ -6,29 +6,26 @@ import com.WWI16AMA.backend_api.Member.MemberRepository;
 import com.WWI16AMA.backend_api.Member.Status;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.jayway.jsonpath.spi.json.GsonJsonProvider;
 import org.hamcrest.collection.IsCollectionWithSize;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.json.GsonJsonParser;
-import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.json.Jackson2JsonEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -46,8 +43,10 @@ public class ApplicationTests {
 
         long found = memberRepository.count();
 
-        Address adr = new Address(25524, "Itzehoe", "Twietbergstraße", 53);
-        Member mem = new Member("Karl", "Hansen", LocalDate.of(1996, Month.DECEMBER, 21), "male", Status.PASSIVE, "karl.hansen@mail.com", adr, "123456789", false);
+        Address adr = new Address(25524, "Itzehoe", "Twietbergstraße 53");
+        Member mem = new Member("Karl", "Hansen",
+                LocalDate.of(1996, Month.DECEMBER, 21), "male", Status.PASSIVE,
+                "karl.hansen@mail.com", adr, "123456789", false);
         memberRepository.save(mem);
 
         assertThat(memberRepository.count()).isEqualTo(found + 1);
@@ -59,36 +58,104 @@ public class ApplicationTests {
         long found = memberRepository.count();
         String limit = found != 0 ? Long.toString(found) : "1337";
 
-        System.out.println("\r\n\tfound:"+found+"\r\n\r\n");
-
         this.mockMvc.perform(get("/members").param("limit", limit))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").isArray())
                 .andExpect(jsonPath("$.content", IsCollectionWithSize.hasSize((int) found)));
-
-
-//        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-//                .andExpect(jsonPath("$.content").value("Hello, World!"));
     }
 
-//    @Test
-//	public void testPostMemberController() throws Exception{
-//
-//	    long found = memberRepository.count();
-//
-//        Address adr = new Address(12345, "Hamburg", "Hafenstraße", 5);
-//        Member mem = new Member("Kurt", "Krömer", LocalDate.of(1975, Month.DECEMBER, 2), "male", Status.PASSIVE, "karl.hansen@mail.com", adr, "123456789", false);
-//
-//
-//        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-//        String json = ow.writeValueAsString(mem);
-//
-//        this.mockMvc.perform(post("/members")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json)).andExpect(status().isOk());
-//
-//        assertThat(memberRepository.count()).isEqualTo(found +1);
-//    }
+    @Test
+    public void testDeleteMemberController() throws Exception {
+
+        long found = memberRepository.count();
+
+        Address adr = new Address(25524, "Itzehoe", "Twietbergstraße 53");
+        Member mem = new Member("Hauke", "Haien",
+                LocalDate.of(1796, Month.DECEMBER, 3), "male", Status.PASSIVE,
+                "karl.hansen@mail.com", adr, "123456789", false);
+        Integer id = memberRepository.save(mem).getId();
+
+        this.mockMvc.perform(delete("/members/" + id))
+                .andExpect(status().isNoContent());
+
+        assertThat(found).isEqualTo(memberRepository.count());
+
+
+        this.mockMvc.perform(delete("/members/" + this.getUnusedId(memberRepository)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void testPostMemberController() throws Exception {
+
+        long found = memberRepository.count();
+
+        Address adr = new Address(12345, "Hamburg", "Hafenstraße 5");
+        Member mem = new Member("Kurt", "Krömer",
+                LocalDate.of(1975, Month.DECEMBER, 2), "male", Status.PASSIVE,
+                "karl.hansen@mail.com", adr, "123456789", false);
+
+
+        this.mockMvc.perform(post("/members")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.marshal(mem))).andExpect(status().isOk());
+
+        assertThat(memberRepository.count()).isEqualTo(found + 1);
+    }
+
+    @Test
+    public void testPutMemberController() throws Exception {
+
+        long found = memberRepository.count();
+
+        Address adr = new Address(54231, "Krefeld", "Bühnenstraße 5");
+        Member mem = new Member("Kurt", "Prödel",
+                LocalDate.of(1975, Month.MAY, 10), "male", Status.PASSIVE,
+                "karl.hansen@mail.com", adr,
+                "123456789", false);
+
+        memberRepository.save(mem);
+
+        Address newAddr = new Address(12345, "Neustadt", "Neustraße 5");
+        mem.setAddress(newAddr);
+        this.mockMvc.perform(put("/members/" + mem.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.marshal(mem)))
+                .andExpect(status().isNoContent());
+        assertThat(Optional.of(newAddr)).isEqualTo(memberRepository.findById(mem.getId()));
+
+        mem.setAddress(null);
+        this.mockMvc.perform(put("members/" + mem.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.marshal(mem)))
+                .andExpect(status().isBadRequest());
+
+        this.mockMvc.perform(put("members/" + this.getUnusedId(memberRepository))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(this.marshal(mem)))
+                .andExpect(status().isNotFound());
+
+    }
+
+    /**
+     * returns an id from an repository, which points to no entry, so repository.existsById(unusedId) returns false.
+     *
+     * @param repository
+     * @return unusedId
+     */
+    private int getUnusedId(CrudRepository repository) {
+        //get random id with no associated member
+        int randomId;
+        Random random = new Random();
+        do {
+            randomId = random.nextInt();
+        } while (repository.existsById(randomId));
+        return randomId;
+    }
+
+    private String marshal(Object o) throws com.fasterxml.jackson.core.JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(o);
+    }
 
 }
