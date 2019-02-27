0b3e31a20e4772b012de157010912f2c33e0fcd4
will-code-for-food-and-water <matthis@posteo.de>
2019-02-27 11:53:21 +0100

    CORS refactort

--- src/main/java/com/WWI16AMA/backend_api/AppConfiguration.java
+++ src/main/java/com/WWI16AMA/backend_api/AppConfiguration.java
@@ -1,29 +1,22 @@
 package com.WWI16AMA.backend_api;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class AppConfiguration {
+public class AppConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
+    final String[] origins = {"http://localhost", "http://localhost:4200",
+            "https://localhost", "https://localhost:4200",
+            "http://wwi16ama.feste-ip.net", "https://wwi16ama.feste-ip.net"};
+    final String[] methods = {"GET", "POST", "PUT", "DELETE"};
 
-            final String[] origins = {"http://localhost", "http://localhost:4200",
-                    "https://localhost", "https://localhost:4200",
-                    "http://wwi16ama.feste-ip.net", "https://wwi16ama.feste-ip.net"};
-            final String[] methods = {"GET", "POST", "PUT", "DELETE"};
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(origins)
-                        .allowedMethods(methods);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(origins)
+                .allowedMethods(methods);
     }
+
 }
