1635c8526ba1137d7bd234b3a83e6463147a8ad5
will-code-for-food-and-water <matthis@posteo.de>
2019-03-14 17:09:00 +0100

    cors sollte funzen

--- src/main/java/com/WWI16AMA/backend_api/AppSecurityConfiguration.java
+++ src/main/java/com/WWI16AMA/backend_api/AppSecurityConfiguration.java
@@ -10,11 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -35,29 +30,17 @@ public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
+
         http
                 .cors()
-                .and().authorizeRequests()
+                .and()
+                .authorizeRequests()
                 .antMatchers("/**").authenticated()
                 .and()
                 .httpBasic()
                 .and()
                 .formLogin().permitAll();
     }
-    final String[] origins = {"http://localhost", "http://localhost:4200",
-            "https://localhost", "https://localhost:4200",
-            "http://wwi16ama.feste-ip.net", "https://wwi16ama.feste-ip.net"};
-    final String[] methods = {"GET", "POST", "PUT", "DELETE"};
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList(origins));
-//        configuration.setAllowedMethods(Arrays.asList(methods));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
