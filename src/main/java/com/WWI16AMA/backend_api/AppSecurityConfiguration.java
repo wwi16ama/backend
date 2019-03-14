7052c9fdceb933ecc4f6cfcf637272aeacac3f5d
will-code-for-food-and-water <matthis@posteo.de>
2019-03-14 16:20:00 +0100

    cors mit spring-sec regeln

--- src/main/java/com/WWI16AMA/backend_api/AppSecurityConfiguration.java
+++ src/main/java/com/WWI16AMA/backend_api/AppSecurityConfiguration.java
@@ -10,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -36,6 +41,20 @@ public class AppSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .formLogin().permitAll();
+    }
+    final String[] origins = {"http://localhost", "http://localhost:4200",
+            "https://localhost", "https://localhost:4200",
+            "http://wwi16ama.feste-ip.net", "https://wwi16ama.feste-ip.net"};
+    final String[] methods = {"GET", "POST", "PUT", "DELETE"};
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(origins));
+        configuration.setAllowedMethods(Arrays.asList(methods));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
