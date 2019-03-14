aeb54aff524e6ee23f371b26ec33274d80cb1eec
will-code-for-food-and-water <matthis@posteo.de>
2019-03-14 16:09:28 +0100

    cors entfernen

--- src/main/java/com/WWI16AMA/backend_api/AppConfiguration.java
+++ src/main/java/com/WWI16AMA/backend_api/AppConfiguration.java
@@ -7,16 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class AppConfiguration implements WebMvcConfigurer {
 
-    final String[] origins = {"http://localhost", "http://localhost:4200",
-            "https://localhost", "https://localhost:4200",
-            "http://wwi16ama.feste-ip.net", "https://wwi16ama.feste-ip.net"};
-    final String[] methods = {"GET", "POST", "PUT", "DELETE"};
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(origins)
-                .allowedMethods(methods);
-    }
+//    final String[] origins = {"http://localhost", "http://localhost:4200",
+//            "https://localhost", "https://localhost:4200",
+//            "http://wwi16ama.feste-ip.net", "https://wwi16ama.feste-ip.net"};
+//    final String[] methods = {"GET", "POST", "PUT", "DELETE"};
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(origins)
+//                .allowedMethods(methods);
+//    }
 
 }
