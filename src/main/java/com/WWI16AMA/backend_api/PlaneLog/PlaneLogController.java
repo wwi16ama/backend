3df4f0b701ef64c4619dc0a48121b4df8f75d78a
HerrWeiss <43146307+HerrWeiss@users.noreply.github.com>
2019-04-01 15:48:54 +0200

    PlaneLog weitergearbeitet
    - Memberüberprüfung und Fehlermeldung, wenn ID falsch
    - BETANKUNGSKOSTENERSTATTUNG als FeeType hinzugefügt
    - Kostenabbuchung findet nun beim erstellen eines PlanlogEntrys automatisch statt

--- src/main/java/com/WWI16AMA/backend_api/PlaneLog/PlaneLogController.java
+++ src/main/java/com/WWI16AMA/backend_api/PlaneLog/PlaneLogController.java
@@ -49,12 +49,13 @@ public class PlaneLogController {
             throw new IllegalArgumentException("Refuel Date cant be in future.");
         }
     Integer memberId = entry.getMemberId();
+        //Member member = memberRepository.findById(entry.getMemberId()).orElseThrow(() -> new NoSuchElementException("Member with the id " + entry.getMemberId() + " does not exist"));
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() ->
                 new NoSuchElementException("Member with the id " + memberId + " does not exist"));
        publisher.publishEvent(new IntTransactionEvent(
                member.getMemberBankingAccount(),
-               new Transaction(entry.getTotalPrice(), Transaction.FeeType.BETANKUNGSKOSTENERSTATTUNG)));
+               new Transaction(entry.getTotalPrice(), Transaction.FeeType.BETANKUNGSKOSTEN)));
         plane.addPlaneLogEntry(entry);
 
 
