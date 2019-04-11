b0dcc3753a21550fbbc360592b7234b4ff4d720d
Jannis <jannis7@justmail.de>
2019-04-11 15:00:55 +0200

    Added FlightPrice for Entrys in Application

--- src/main/java/com/WWI16AMA/backend_api/Application.java
+++ src/main/java/com/WWI16AMA/backend_api/Application.java
@@ -204,15 +204,19 @@ public class Application extends SpringBootServletInitializer {
         PilotLogEntry ple1 = new PilotLogEntry("D-ERFI", "Reilingen", LocalDateTime.of(2019, Month.FEBRUARY,
                 15, 10, 30), "Mannheim", LocalDateTime.of(2019, Month.FEBRUARY,
                 15, 10, 45), true, 1);
+        ple1.setFlightPrice(30.0);
         PilotLogEntry ple2 = new PilotLogEntry("D-EJEK", "Mannheim", LocalDateTime.of(2019, Month.FEBRUARY,
                 20, 10, 00), "Berlin", LocalDateTime.of(2019, Month.FEBRUARY,
                 20, 13, 45), true, 2);
+        ple2.setFlightPrice(15.0);
         PilotLogEntry ple3 = new PilotLogEntry("D-EJEK", "Berlin", LocalDateTime.of(2019, Month.MARCH,
                 03, 10, 00), "Reilingen", LocalDateTime.of(2019, Month.MARCH,
                 03, 14, 30), true, 3);
+        ple3.setFlightPrice(22.5);
         PilotLogEntry ple4 = new PilotLogEntry("D-EJEK", "Reilingen", LocalDateTime.of(2019, Month.MARCH,
                 20, 12, 00), "Reilingen", LocalDateTime.of(2019, Month.MARCH,
                 20, 13, 00), false, 1);
+        ple4.setFlightPrice(47.11);
 
         PilotLogEntry[] pilotLogEntries = {ple1, ple2, ple3, ple4};
         for (PilotLogEntry entry : pilotLogEntries) {
