a63cb894f52745a8c2e2827ffbd7bce8d7d66de5
Matthis Gördel <matthis@posteo.de>
2019-04-25 16:33:38 +0200

    missing annotation

--- src/main/java/com/WWI16AMA/backend_api/PlaneLog/PlaneLogRepository.java
+++ src/main/java/com/WWI16AMA/backend_api/PlaneLog/PlaneLogRepository.java
@@ -1,6 +1,8 @@
 package com.WWI16AMA.backend_api.PlaneLog;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlaneLogRepository extends PagingAndSortingRepository<PlaneLogEntry, Long> {
 }
